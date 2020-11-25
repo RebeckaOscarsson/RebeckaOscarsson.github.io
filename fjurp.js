faff736931cc0feba1f4573f8bb540be65ca051d
RebeckaOscarsson <70890347+RebeckaOscarsson@users.noreply.github.com>
2020-11-25 18:53:35 +0100

    startat färgbytesfunktion

--- fjurp.js
+++ fjurp.js
@@ -7,6 +7,7 @@ document.querySelector("img.lightswitch").addEventListener("click", lightswitch)
 
 
 function changeVideo(videoName) {
+    document.getElementById("color").style.display = "none";
     var tvChannel = document.getElementById("tvChannel");
     tvChannel.style.visibility = "visible";
     tvChannel.src = videoName;
@@ -16,7 +17,6 @@ function changeVideo(videoName) {
     if (videoName == "icebells.mp4") {
         showSub("pizzacats.vtt")
     }
-
 }
 
 
@@ -47,7 +47,7 @@ function turnOff() {
 var button = document.getElementById("button1");
 button.addEventListener("click", function () {
     changeVideo("icebells.mp4");
-}); //jag gör en anonym funktion för annars kan jag inte skicka in en parameter. Tydligen är detta en fullösning.
+}); //jag gör en anonym funktion för annars kan jag inte skicka in en parameter.
 
 
 var button2 = document.getElementById("button2");
@@ -69,6 +69,14 @@ var button5 = document.getElementById("button5");
 button5.addEventListener("click", function () {
     changeVideo("green.mp4");
 });
+
+var button6 = document.getElementById("button6");
+button6.addEventListener("click", function () {
+    turnOff();document.getElementById("color").style.display = "block";
+});
+
+
+
 
 var buttonOff = document.getElementById("buttonOff");
 buttonOff.addEventListener("click", changeBg);
@@ -94,4 +102,177 @@ function loadPainting() {
     }
 }
 
+
+
+
+function valjfarg() {
+    let valdfarg = document.querySelector("input.color").value;
+    let fargenfinns = false
+    while (fargenfinns == false) {
+        for (let i = 0; i < CSS_COLOR_NAMES.length; i++) {
+
+            if (valdfarg.toUpperCase() == CSS_COLOR_NAMES[i].toUpperCase()) {
+                document.body.style.backgroundColor = valdfarg;
+                fargenfinns = true;
+                break;
+            }
+        }
+        if (fargenfinns == false) {
+            alert("färgen finns inte");
+            break;
+        }
+    }
+}
+
 loadPainting()
+
+const CSS_COLOR_NAMES = [
+    "AliceBlue",
+    "AntiqueWhite",
+    "Aqua",
+    "Aquamarine",
+    "Azure",
+    "Beige",
+    "Bisque",
+    "Black",
+    "BlanchedAlmond",
+    "Blue",
+    "BlueViolet",
+    "Brown",
+    "BurlyWood",
+    "CadetBlue",
+    "Chartreuse",
+    "Chocolate",
+    "Coral",
+    "CornflowerBlue",
+    "Cornsilk",
+    "Crimson",
+    "Cyan",
+    "DarkBlue",
+    "DarkCyan",
+    "DarkGoldenRod",
+    "DarkGray",
+    "DarkGrey",
+    "DarkGreen",
+    "DarkKhaki",
+    "DarkMagenta",
+    "DarkOliveGreen",
+    "DarkOrange",
+    "DarkOrchid",
+    "DarkRed",
+    "DarkSalmon",
+    "DarkSeaGreen",
+    "DarkSlateBlue",
+    "DarkSlateGray",
+    "DarkSlateGrey",
+    "DarkTurquoise",
+    "DarkViolet",
+    "DeepPink",
+    "DeepSkyBlue",
+    "DimGray",
+    "DimGrey",
+    "DodgerBlue",
+    "FireBrick",
+    "FloralWhite",
+    "ForestGreen",
+    "Fuchsia",
+    "Gainsboro",
+    "GhostWhite",
+    "Gold",
+    "GoldenRod",
+    "Gray",
+    "Grey",
+    "Green",
+    "GreenYellow",
+    "HoneyDew",
+    "HotPink",
+    "IndianRed",
+    "Indigo",
+    "Ivory",
+    "Khaki",
+    "Lavender",
+    "LavenderBlush",
+    "LawnGreen",
+    "LemonChiffon",
+    "LightBlue",
+    "LightCoral",
+    "LightCyan",
+    "LightGoldenRodYellow",
+    "LightGray",
+    "LightGrey",
+    "LightGreen",
+    "LightPink",
+    "LightSalmon",
+    "LightSeaGreen",
+    "LightSkyBlue",
+    "LightSlateGray",
+    "LightSlateGrey",
+    "LightSteelBlue",
+    "LightYellow",
+    "Lime",
+    "LimeGreen",
+    "Linen",
+    "Magenta",
+    "Maroon",
+    "MediumAquaMarine",
+    "MediumBlue",
+    "MediumOrchid",
+    "MediumPurple",
+    "MediumSeaGreen",
+    "MediumSlateBlue",
+    "MediumSpringGreen",
+    "MediumTurquoise",
+    "MediumVioletRed",
+    "MidnightBlue",
+    "MintCream",
+    "MistyRose",
+    "Moccasin",
+    "NavajoWhite",
+    "Navy",
+    "OldLace",
+    "Olive",
+    "OliveDrab",
+    "Orange",
+    "OrangeRed",
+    "Orchid",
+    "PaleGoldenRod",
+    "PaleGreen",
+    "PaleTurquoise",
+    "PaleVioletRed",
+    "PapayaWhip",
+    "PeachPuff",
+    "Peru",
+    "Pink",
+    "Plum",
+    "PowderBlue",
+    "Purple",
+    "RebeccaPurple",
+    "Red",
+    "RosyBrown",
+    "RoyalBlue",
+    "SaddleBrown",
+    "Salmon",
+    "SandyBrown",
+    "SeaGreen",
+    "SeaShell",
+    "Sienna",
+    "Silver",
+    "SkyBlue",
+    "SlateBlue",
+    "SlateGray",
+    "SlateGrey",
+    "Snow",
+    "SpringGreen",
+    "SteelBlue",
+    "Tan",
+    "Teal",
+    "Thistle",
+    "Tomato",
+    "Turquoise",
+    "Violet",
+    "Wheat",
+    "White",
+    "WhiteSmoke",
+    "Yellow",
+    "YellowGreen",
+];
