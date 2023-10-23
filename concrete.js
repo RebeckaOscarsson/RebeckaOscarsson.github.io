f4492453d57812cdecd6be6cecbcd516cf0db96e
rebecka-oscarsson <rebecka.oscarsson@gmail.com>
2023-10-23 19:49:02 +0200

    Add color filters animation

--- concrete.js
+++ concrete.js
@@ -1,15 +1,21 @@
 //global variabel
-let speedClass="";
+let speedClass=null;
+let colorClass=null;
 
 //html-element
 const sheepWindow = document.querySelector(".sheepWindow");
+const cowWindow = document.querySelector(".cowWindow");
 
 //eventlisteners
 sheepWindow.addEventListener("click", changeSpeed);
+cowWindow.addEventListener("click", changeColor);
 
 
 function changeSpeed() { 
     switch (speedClass) {
+        case null:
+            speedClass="slow";
+            break;
         case "slow":
             speedClass="medium";
             break;
@@ -17,9 +23,28 @@ function changeSpeed() {
             speedClass="fast"
             break;       
         default:
-            speedClass="slow"
+            speedClass=null
             break
     }
     sheepWindow.classList.value = 'sheepWindow'  
     sheepWindow.classList.add(speedClass);
+}
+
+function changeColor() { 
+    switch (colorClass) {
+        case null:
+            colorClass="color1";
+            break;
+        case "color1":
+            colorClass="color2";
+            break;
+        case "color2":
+            colorClass="color3"
+            break;       
+        default:
+            colorClass=null
+            break
+    }
+    cowWindow.classList.value = 'cowWindow'  
+    cowWindow.classList.add(colorClass);
 }
